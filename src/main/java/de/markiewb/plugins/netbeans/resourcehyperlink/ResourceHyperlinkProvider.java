72bf50342ddea2d5bf7e72f3efd2e3437aacc96b
markiewb <benno.markiewicz@googlemail.com>
2013-10-01 23:36:10 +0200

    support files from project source root, ask user to choose from list if multiple matches

--- src/main/java/de/markiewb/plugins/netbeans/resourcehyperlink/ResourceHyperlinkProvider.java
+++ src/main/java/de/markiewb/plugins/netbeans/resourcehyperlink/ResourceHyperlinkProvider.java
@@ -1,21 +1,33 @@
 package de.markiewb.plugins.netbeans.resourcehyperlink;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.EnumSet;
+import java.util.List;
 import java.util.Set;
+import javax.swing.JComboBox;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 import javax.swing.text.JTextComponent;
 import org.netbeans.api.editor.mimelookup.MimeRegistration;
 import org.netbeans.api.java.lexer.JavaTokenId;
+import org.netbeans.api.java.project.JavaProjectConstants;
 import org.netbeans.api.lexer.Token;
 import org.netbeans.api.lexer.TokenHierarchy;
 import org.netbeans.api.lexer.TokenSequence;
+import org.netbeans.api.project.FileOwnerQuery;
+import org.netbeans.api.project.Project;
+import org.netbeans.api.project.ProjectUtils;
+import org.netbeans.api.project.SourceGroup;
+import org.netbeans.api.project.Sources;
 import org.netbeans.editor.BaseDocument;
 import org.netbeans.editor.Utilities;
-import org.netbeans.lib.editor.hyperlink.spi.HyperlinkProvider;
 import org.netbeans.lib.editor.hyperlink.spi.HyperlinkProviderExt;
 import org.netbeans.lib.editor.hyperlink.spi.HyperlinkType;
 import org.netbeans.modules.editor.NbEditorUtilities;
+import org.openide.DialogDescriptor;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
 import org.openide.awt.StatusDisplayer;
 import org.openide.cookies.EditCookie;
 import org.openide.cookies.OpenCookie;
@@ -24,14 +36,12 @@ import org.openide.loaders.DataObject;
 import org.openide.loaders.DataObjectNotFoundException;
 import org.openide.util.Exceptions;
 
-
 @MimeRegistration(mimeType = "text/x-java", service = HyperlinkProviderExt.class)
 public class ResourceHyperlinkProvider implements HyperlinkProviderExt {
 
     private int startOffset;
     private int endOffset;
 
-    private FileObject fileToOpen;
     private String linkTarget;
 
     @Override
@@ -77,17 +87,17 @@ public class ResourceHyperlinkProvider implements HyperlinkProviderExt {
                     endOffset = startOffset;
                 }
                 final String wholeText = resourceToken.text().subSequence(1, resourceToken.length() - 1).toString();
-                
-                final int offSetAtCursor = (offset-startOffset)+1;
+
+                final int offSetAtCursor = (offset - startOffset) + 1;
                 int indexOf = getIndexOfNextPathSeparator(wholeText, offSetAtCursor);
-                if (indexOf<0){
-                    indexOf=wholeText.length();
+                if (indexOf < 0) {
+                    indexOf = wholeText.length();
                 }
-                endOffset=startOffset+Math.min(indexOf+1, wholeText.length());
-                final String innerSelectedText = resourceToken.text().subSequence(1, 1+Math.min(indexOf+1, wholeText.length())).toString();
+                endOffset = startOffset + Math.min(indexOf + 1, wholeText.length());
+                final String innerSelectedText = resourceToken.text().subSequence(1, 1 + Math.min(indexOf + 1, wholeText.length())).toString();
                 linkTarget = innerSelectedText;
-                
-                StatusDisplayer.getDefault().setStatusText("Path :" + startOffset + "/"+endOffset+"/"+offset + "//"+(offset-startOffset)+"="+innerSelectedText);
+
+                StatusDisplayer.getDefault().setStatusText("Path :" + startOffset + "/" + endOffset + "/" + offset + "//" + (offset - startOffset) + "=" + innerSelectedText);
                 return true;
             }
 
@@ -98,15 +108,48 @@ public class ResourceHyperlinkProvider implements HyperlinkProviderExt {
     }
 
     private int getIndexOfNextPathSeparator(final String wholeText, final int offSetAtCursor) {
-        int indexOf = wholeText.indexOf("/", offSetAtCursor-1);
-        if (indexOf<0){
+        int indexOf = wholeText.indexOf("/", offSetAtCursor - 1);
+        if (indexOf < 0) {
             //TODO fallback use windows separator
 //            indexOf = wholeText.indexOf("\\", offSetAtCursor-1);
         }
         return indexOf;
     }
 
-    private FileObject findResourceFileObject(Document doc, String path) {
+     private List<FileObject> findFiles(Document doc, String path) {
+                 final FileObject fileInCurrentDirectory = getMatchingFileInCurrentDirectory(doc, path);
+
+
+        //fallback to search in all source roots
+        FileObject docFO = NbEditorUtilities.getFileObject(doc);
+        List<FileObject> foundMatches =new ArrayList<FileObject>();
+                
+        foundMatches.addAll(getMatchingFilesFromSourceRoots(FileOwnerQuery.getOwner(docFO), path));
+        
+        if (null != fileInCurrentDirectory) {
+            foundMatches.add(fileInCurrentDirectory);
+        }
+        return foundMatches;
+     }
+
+
+    private List<FileObject> getMatchingFilesFromSourceRoots(Project p, String path) {
+        List<SourceGroup> list = new ArrayList<SourceGroup>();
+        List<FileObject> foundMatches = new ArrayList<FileObject>();
+        final Sources sources = ProjectUtils.getSources(p);
+        list.addAll(Arrays.asList(sources.getSourceGroups(JavaProjectConstants.SOURCES_TYPE_JAVA)));
+        list.addAll(Arrays.asList(sources.getSourceGroups(JavaProjectConstants.SOURCES_TYPE_RESOURCES)));
+        list.addAll(Arrays.asList(sources.getSourceGroups(JavaProjectConstants.SOURCES_HINT_TEST)));
+        for (SourceGroup sourceGroup : list) {
+            FileObject fileObject = sourceGroup.getRootFolder().getFileObject(path);
+            if (fileObject != null) {
+                foundMatches.add(fileObject);
+            }
+        }
+        return foundMatches;
+    }
+
+    private FileObject getMatchingFileInCurrentDirectory(Document doc, String path) {
         FileObject docFO = NbEditorUtilities.getFileObject(doc);
         FileObject currentDir = docFO.getParent();
         return currentDir.getFileObject(path);
@@ -119,12 +162,39 @@ public class ResourceHyperlinkProvider implements HyperlinkProviderExt {
 
     @Override
     public void performClickAction(Document doc, int position, HyperlinkType type) {
-        fileToOpen = findResourceFileObject(doc, linkTarget);
+        List<FileObject> foundMatches = findFiles(doc, linkTarget);
+        final Project project = FileOwnerQuery.getOwner(NbEditorUtilities.getFileObject(doc));        
+        FileObject fileToOpen = getSingleMatchOrAskForUserChoice(foundMatches, project);        
+
         if (fileToOpen == null) {
             StatusDisplayer.getDefault().setStatusText("Invalid path :" + linkTarget);
             return;
         }
         openInEditor(fileToOpen);
+    }
+
+    private FileObject getSingleMatchOrAskForUserChoice(List<FileObject> foundMatches, Project project) {
+        if (foundMatches.size() == 1) {
+            return foundMatches.get(0);
+        }
+        if (foundMatches.size() >= 2) {
+            List<String> list1 = new ArrayList<String>();
+
+            for (FileObject fileObject : foundMatches) {
+                //convert absolute path to relative regarding the project
+                String path1 = fileObject.getPath().substring(project.getProjectDirectory().getPath().length());
+                list1.add(path1);
+            }
+            String[] strings = list1.toArray(new String[list1.size()]);
+
+            final JComboBox<String> jList = new JComboBox<String>(strings);
+            if (DialogDisplayer.getDefault().notify(new DialogDescriptor(jList, "Multiple files found. Please choose:")) == NotifyDescriptor.OK_OPTION) {
+                return foundMatches.get(jList.getSelectedIndex());
+            } else {
+                //
+            }
+        }
+        return null;
     }
 
     public static void openInEditor(FileObject fileToOpen) {
@@ -174,7 +244,7 @@ public class ResourceHyperlinkProvider implements HyperlinkProviderExt {
     public String getTooltipText(Document doc, int offset, HyperlinkType type) {
         //no tooltip until https://netbeans.org/bugzilla/show_bug.cgi?id=236249 is resolved
         return null;
-        
+
 //        //call calculation first, because the order of method calls in undefined (by API doc)
 //        boolean hyperlinkPoint = isHyperlinkPoint(doc, offset, type);
 //         StatusDisplayer.getDefault().setStatusText(""+offset+"/"+type);
