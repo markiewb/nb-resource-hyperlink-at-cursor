b767fb4d757bb2c25d111a3e0f7a07e5e19fac7a
markiewb <benno.markiewicz@googlemail.com>
2013-12-19 00:19:32 +0100

    [doc] add documentation, license and screenshot [pom] configure signing [refactor] rename module to apply to other plugins [doc] state that it only supports Java sources

---
+++ src/test/java/NewMain.java
@@ -0,0 +1,14 @@
+import java.math.BigDecimal;
+
+public class NewMain {
+
+    public static void main(String[] args) {
+        System.out.println("filename = "
+                + "com/toy/anagrams/lib/WordLibrary.java");
+        System.out.println("filename = " + "WordLibrary.java");
+        System.out.println("D:\\ws\\nb-resource-hyperlink-at-cursor\\doc\\screenshot-1.0.0.png");
+        System.out.println("D:/ws/nb-resource-hyperlink-at-cursor/doc/screenshot-1.0.0.png");
+        System.out.println("filename = " + 1 + "help.png"
+                + new BigDecimal("0"));
+    }
+}
